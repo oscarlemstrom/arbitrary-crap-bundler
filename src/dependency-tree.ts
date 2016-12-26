8ef85f4f127fd41f35d66a8998da304c68a45fa9
Oscar Lemstrom <oscar.lemstrom@frantic.com>
2016-12-26 11:29:51 +0200

    Create initial Tree and Node data structures

---
+++ src/dependency-tree.ts
@@ -0,0 +1,70 @@
+export interface INode<T>  {
+    connect(n: INode<T>): void;
+    disconnect(n: INode<T>): void;
+    depth(currentDepth?: number);
+    flatten(values? : T[])
+    value: T;
+}
+
+export interface ITree<T> {
+    setRoot(n: INode<T>): void;
+    uniques(): T[];
+    maxDepth: number;
+}
+
+const uniqueOnly = (value, index, all) => all.indexOf(value) === index;
+
+export class Tree<T> implements ITree<T> {
+    private root: INode<T>;
+
+    setRoot(n: INode<T>) {
+        this.root = n;
+    }
+
+    uniques(): T[] {
+        const values: T[] = [];
+        if(!this.root) return values;
+        return this.root.flatten().filter(uniqueOnly);
+    }
+
+    get maxDepth(): number {
+        if(!this.root) return 0;
+        return this.root.depth();
+    }
+}
+
+export class Node<T> implements INode<T> {
+    private connected: INode<T>[] = [];
+    private val: T;
+
+    constructor(value: T) {
+        this.val = value;
+    }
+
+    get value(): T {
+        return this.val;
+    }
+
+    connect(n: INode<T>) {
+        if(!this.connected.includes(n)) this.connected.push(n);
+    }
+
+    disconnect(n: INode<T>) {
+        const index = this.connected.indexOf(n);
+        if(index > -1) this.connected.splice(index, 1);
+    }
+
+    depth(currentDepth = 0): number {
+        const depths = this.connected.map(n => n.depth(currentDepth + 1));
+        return depths.reduce((d1, d2) => Math.max(d1, d2), currentDepth);
+    }
+
+    flatten(values = []): T[] {
+        const newValues = [...values, this.value];
+        return newValues.concat(
+            this.connected
+                .map(n => n.flatten())
+                .reduce((v1, v2) => v1.concat(v2), [])
+        );
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
-const x: number = 0;
-console.log(x);
+import { Tree, Node } from './dependency-tree';
+
+const dependencyTree = new Tree<string>();
+
+// TEST NODES
+const rootNode = new Node<string>('a');
+const node1_1 = new Node<string>('-b1');
+const node1_2 = new Node<string>('-b2');
+const node2_1 = new Node<string>('--c1');
+const node2_2 = new Node<string>('--c2');
+
+rootNode.connect(node1_1);
+rootNode.connect(node1_2);
+node1_1.connect(node2_1);
+node1_1.connect(node2_2);
+node1_2.connect(node2_1);
+
+dependencyTree.setRoot(rootNode);
+console.log(JSON.stringify(dependencyTree.uniques(), null, 4));
